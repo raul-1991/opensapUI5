b16543b6b3e32dabf8ba94666e10954fe5838c4c
raul-1991 <raul.garcia@in-rgy.com>
2021-02-15 08:06:58 +0000

    new commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,59 @@
+/*
+ * Load the resource Controller and once is loaded, execute the function. THis function has the resource Controller.
+ * From the resource controller we extends the functionallity. For extend we use the namespace and define the events
+ */
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"opensap/myapp/model/formatter"
+], function (Controller, MessageToast, Filter, FilterOperator, formatter) {
+	"use strict";
+
+	return Controller.extend("opensap.myapp.controller.App", {
+
+		formatter: formatter,
+		onShowHello: function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+
+		},
+
+		onFilterProducts: function (oEvent) {
+
+			// build filter array
+			var aFilter = [],
+				sQuery = oEvent.getParameter("query"),
+				// retrieve list control
+				oList = this.getView().byId("productsList"),
+				// get binding for aggregation 'items'
+				oBinding = oList.getBinding("items");
+
+			if (sQuery) {
+				aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+			}
+			// apply filter. an empty filter array simply removes the filter
+			// which will make all entries visible again
+			oBinding.filter(aFilter);
+		},
+		onItemSelected: function (oEvent) {
+			var oSelectedItem = oEvent.getSource();
+			var oContext = oSelectedItem.getBindingContext();
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+
+			oProductDetailPanel.bindElement({
+				path: sPath
+			});
+			this.byId("productDetailsPanel").setVisible(true);
+		}
+
+	});
+
+});
